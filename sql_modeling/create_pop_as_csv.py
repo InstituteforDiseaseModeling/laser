d7202b185aa602e56179a8bb4cbcd52d5fd081f0
Jonathan Bloedow <jhb4@cornell.edu>
2024-01-05 12:57:05 -0800

    Added utility to create initial population as csv file.

---
+++ sql_modeling/create_pop_as_csv.py
@@ -0,0 +1,19 @@
+import sqlite3
+import csv
+import settings
+from sir_sql import initialize_database
+
+conn = sqlite3.connect(":memory:")  # Use in-memory database for simplicity
+initialize_database( conn )
+
+cursor = conn.cursor()
+get_all_query = "SELECT * FROM agents"
+cursor.execute( get_all_query )
+rows = cursor.fetchall()
+
+conn.close()
+
+with open( settings.pop_file_out, "w", newline='' ) as csvfile:
+    csv_writer = csv.writer( csvfile )
+    csv_writer.writerow( ['id', 'node', 'age', 'infected', 'infection_timer', 'incubation_timer', 'immunity', 'immunity_timer' ] )
+    csv_writer.writerows( rows )
