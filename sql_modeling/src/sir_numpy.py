7168aa8acf23992c16a8098a910d0284cf290d32
Jonathan Bloedow <jhb4@cornell.edu>
2024-01-11 17:11:49 -0800

    Ported all numpy 'step functions' over to C modules using nice-and-simple ctypes intrface. Cuts time in half. Still need to port report.

--- sql_modeling/src/sir_numpy.py
+++ sql_modeling/src/sir_numpy.py
@@ -3,6 +3,7 @@ import csv
 import numpy as np
 import concurrent.futures
 from functools import partial
+import numba
 import pdb
 
 import settings
@@ -10,7 +11,8 @@ import report
 
 # Globals! (not really)
 #base_infectivity = 0.000002
-settings.base_infectivity = 0.00001
+#settings.base_infectivity = 0.00001
+settings.base_infectivity = 0.0001
 
 def load( pop_file ):
     """
@@ -29,7 +31,10 @@ def load( pop_file ):
     columns = {header: data[:, i] for i, header in enumerate(headers)}
     columns['infected'] = columns['infected'].astype(bool)
     columns['immunity'] = columns['immunity'].astype(bool)
-    columns['node'] = columns['node'].astype(int)
+    columns['node'] = columns['node'].astype(np.uint32)
+    columns['infection_timer'] = columns['infection_timer'].astype(np.float32) # int better?
+    columns['incubation_timer'] = columns['incubation_timer'].astype(np.float32) # int better?
+    columns['immunity_timer'] = columns['immunity_timer'].astype(np.float32) # int better?
 
     settings.pop = len(columns['infected'])
     print( f"Population={settings.pop}" )
@@ -37,6 +42,12 @@ def load( pop_file ):
     settings.num_nodes = len(settings.nodes)
     print( f"Nodes={settings.num_nodes}" )
     # Now 'columns' is a dictionary where keys are column headers and values are NumPy arrays
+    def eula():
+        # test out what happens if we render big chunks of the population epi-borrowing
+        condition = np.logical_and(~columns['infected'], columns['age']>5)
+        columns['immunity'][condition] = 1
+        columns['immunity_timer'][condition] = -1
+    eula()
     return columns
 
 def initialize_database():
@@ -74,48 +85,144 @@ def collect_report( data ):
     #print( "Stop timestep report." )
     return infected_counts, susceptible_counts, recovered_counts
 
+import ctypes
+# Load the shared library
+update_ages_lib = ctypes.CDLL('./update_ages.so')
+# Define the function signature
+update_ages_lib.update_ages.argtypes = [np.ctypeslib.ndpointer(dtype=np.double, flags='C_CONTIGUOUS'), ctypes.c_size_t]
+update_ages_lib.progress_infections.argtypes = [
+    ctypes.c_size_t,  # n
+    np.ctypeslib.ndpointer(dtype=np.float32, flags='C_CONTIGUOUS'),  # infection_timer
+    np.ctypeslib.ndpointer(dtype=np.float32, flags='C_CONTIGUOUS'),  # incubation_timer
+    np.ctypeslib.ndpointer(dtype=bool, flags='C_CONTIGUOUS'),  # infected
+    np.ctypeslib.ndpointer(dtype=np.float32, flags='C_CONTIGUOUS'),  # immunity_timer
+    np.ctypeslib.ndpointer(dtype=bool, flags='C_CONTIGUOUS'),  # immunity
+]
+update_ages_lib.progress_immunities.argtypes = [
+    ctypes.c_size_t,  # n
+    np.ctypeslib.ndpointer(dtype=np.float32, flags='C_CONTIGUOUS'),  # immunity_timer
+    np.ctypeslib.ndpointer(dtype=bool, flags='C_CONTIGUOUS'),  # immunity
+]
+update_ages_lib.calculate_new_infections.argtypes = [
+    ctypes.c_size_t,  # n
+    ctypes.c_size_t,  # n
+    np.ctypeslib.ndpointer(dtype=np.float32, flags='C_CONTIGUOUS'),  # incubation_timer
+    np.ctypeslib.ndpointer(dtype=np.uint32, flags='C_CONTIGUOUS'),  # nodes
+    np.ctypeslib.ndpointer(dtype=np.int64, flags='C_CONTIGUOUS'),  # inf_counts
+    np.ctypeslib.ndpointer(dtype=np.int64, flags='C_CONTIGUOUS'),  # sus_counts
+    np.ctypeslib.ndpointer(dtype=np.uint32, flags='C_CONTIGUOUS'),  # new_infections
+]
+update_ages_lib.handle_new_infections.argtypes = [
+    ctypes.c_uint32, # num_agents
+    ctypes.c_uint32, # node
+    np.ctypeslib.ndpointer(dtype=np.uint32, flags='C_CONTIGUOUS'), # nodes
+    np.ctypeslib.ndpointer(dtype=np.bool_, flags='C_CONTIGUOUS'),  # infected
+    np.ctypeslib.ndpointer(dtype=np.bool_, flags='C_CONTIGUOUS'),  # immunity
+    np.ctypeslib.ndpointer(dtype=np.float32, flags='C_CONTIGUOUS'), # incubation_timer
+    np.ctypeslib.ndpointer(dtype=np.float32, flags='C_CONTIGUOUS'), # infection_timer
+    ctypes.c_int # num_new_infections
+]
+update_ages_lib.migrate.argtypes = [
+    ctypes.c_uint32, # num_agents
+    np.ctypeslib.ndpointer(dtype=np.bool_, flags='C_CONTIGUOUS'),  # infected
+    np.ctypeslib.ndpointer(dtype=np.uint32, flags='C_CONTIGUOUS'), # nodes
+]
+
+
 def update_ages( data ):
-    data['age'] += 1/365
+    @numba.jit(parallel=True,nopython=True)
+    def updates_ages_nb( ages ):
+        n = len(ages)
+        for i in range(n):
+            ages[i] += 1/365
+        return ages
+        #data['age'] += 1/365
+    def updates_ages_np( ages ):
+        ages += 1/365
+        return ages
+    def updates_ages_c( ages ):
+        update_ages_lib.update_ages(ages, len(ages))
+        return ages
+
+    data['age'] = updates_ages_c( data['age'] )
     return data
 
 def progress_infections( data ):
-    #pdb.set_trace()
     # Update infected agents
     # infection timer: decrement for each infected person
-    data['infection_timer'][data['infection_timer'] >= 1] -= 1
-    data['incubation_timer'][data['incubation_timer'] >= 1] -= 1
-    # some people clear
-    condition = np.logical_and(data['infected'], data['infection_timer'] == 0)
-    data['infected'][condition] = False
-    # recovereds gain immunity
-    data['immunity_timer'][condition] = np.random.randint(10, 41, size=np.sum(condition))
-    data['immunity'][condition] = True
+    def progress_infections_np( data ):
+        data['infection_timer'][data['infection_timer'] >= 1] -= 1
+        data['incubation_timer'][data['incubation_timer'] >= 1] -= 1
+        # some people clear
+        condition = np.logical_and(data['infected'], data['infection_timer'] == 0)
+        data['infected'][condition] = False
+        # recovereds gain immunity
+        data['immunity_timer'][condition] = np.random.randint(10, 41, size=np.sum(condition))
+        data['immunity'][condition] = True
+
+    def progress_infections_c( data ):
+        update_ages_lib.progress_infections(len(data['age']), data['infection_timer'], data['incubation_timer'], data['infected'], data['immunity_timer'], data['immunity'])
+        return
+
+    progress_infections_c( data )
     return data
 
 # Update immune agents
 def progress_immunities( data ):
-    # immunity decays
-    condition = np.logical_and(data['immunity'], data['immunity_timer'] > 0)
-    data['immunity_timer'][condition] -= 1
-    # Recoverd->Susceptible
-    condition = np.logical_and(data['immunity'], data['immunity_timer'] == 0)
-    data['immunity'][condition] = False
+    def progress_immunities_np( data ):
+        # immunity decays
+        condition = np.logical_and(data['immunity'], data['immunity_timer'] > 0)
+        data['immunity_timer'][condition] -= 1
+        # Recoverd->Susceptible
+        condition = np.logical_and(data['immunity'], data['immunity_timer'] == 0)
+        data['immunity'][condition] = False
+
+    def progress_immunities_c( data ):
+        update_ages_lib.progress_immunities(len(data['age']), data['immunity_timer'], data['immunity'])
+        return
+
+    progress_immunities_c( data )
     return data
 
 def calculate_new_infections( data, inf, sus ):
-    # We want to count the number of incubators by now all at once not in a for loop.
-    node_counts_incubators = np.bincount( data['node'][data['incubation_timer']>=1] )
-    if len( node_counts_incubators ) == 0:
-        print( "node_counts_incubators came back size 0." )
-        node_counts_incubators = np.zeros( settings.num_nodes )
-        #raise ValueError( "node_counts_incubators came back size 0." )
-    sorted_items = sorted(inf.items())
-    inf_np = np.array([value for _, value in sorted_items])
-    foi = (inf_np-node_counts_incubators) * settings.base_infectivity
-    sus_np = np.array(list(sus.values()))
-    new_infections = (foi * sus_np).astype(int)
-    #print( new_infections )
-    return new_infections
+    def calculate_new_infections_np( data, inf, sus ):
+        # We want to count the number of incubators by now all at once not in a for loop.
+        node_counts_incubators = np.zeros(len(inf))
+        node_counts_incubators = np.bincount( data['node'][data['incubation_timer']>=1] )
+        if len( node_counts_incubators ) == 0:
+            print( "node_counts_incubators came back size 0." ) # this can be OK at the beginning.
+            node_counts_incubators = np.zeros( settings.num_nodes )
+        elif len( node_counts_incubators ) != len( inf ):
+            #print( "node_counts_incubators came back missing a node." )
+            node_counts_incubators = np.pad( node_counts_incubators, (0, len(inf)-len(node_counts_incubators) ), 'constant', constant_values=0 )
+
+        sorted_items = sorted(inf.items())
+        inf_np = np.array([value for _, value in sorted_items])
+        if inf_np.shape != node_counts_incubators.shape:
+            raise RuntimeError( f"inf_np shape ({inf_np.shape}) has different size from node_counts_incubators ({node_counts_incubators.shape})." )
+        foi = (inf_np-node_counts_incubators) * settings.base_infectivity
+        sus_np = np.array(list(sus.values()))
+        new_infections = (foi * sus_np).astype(int)
+        #print( new_infections )
+        return new_infections
+    def calculate_new_infections_c( data, inf, sus ):
+        new_infections = np.zeros( len( inf ) ).astype( np.uint32 ) # return variable
+        sorted_items = sorted(inf.items())
+        #inf_np = np.array([value for _, value in sorted_items]).astype(np.uint32)
+        inf_np = np.array([value for _, value in sorted_items])
+        sus_np = np.array(list(sus.values()))
+        update_ages_lib.calculate_new_infections(
+                len(data['age']),
+                len( inf ),
+                data['incubation_timer'],
+                data['node'],
+                inf_np,
+                sus_np,
+                new_infections
+            )
+        return new_infections 
+    #return calculate_new_infections_np( data, inf, sus )
+    return calculate_new_infections_c( data, inf, sus )
 
 def handle_transmission_by_node( data, new_infections, node=0 ):
     # Step 5: Update the infected flag for NEW infectees
@@ -133,44 +240,69 @@ def handle_transmission_by_node( data, new_infections, node=0 ):
 
         # Update the 'infected' column based on the selected indices
         data['infected'][selected_indices] = True
-        data['incubation_timer'][selected_indices] = 2
+        #data['incubation_timer'][selected_indices] = 2
+
+    def handle_new_infections_c(new_infections):
+        update_ages_lib.handle_new_infections(
+                len(data['age']),
+                node,
+                data['node'],
+                data['infected'],
+                data['immunity'],
+                data['incubation_timer'],
+                data['infection_timer'],
+                new_infections
+            )
 
     #print( new_infections[node] )
     if new_infections[node]>0:
-        handle_new_infections(new_infections[node])
+        #handle_new_infections(new_infections[node])
+        handle_new_infections_c(new_infections[node])
         #handle_new_infections(new_infections)
 
     #print( f"{new_infections} new infections in node {node}." )
     return data
 
 def handle_transmission( data_in, new_infections_in ):
-    #print( "handle_transmission" )
-    # We want to do this in parallel
+    # We want to do this in parallel;
     htbn = partial( handle_transmission_by_node, data_in, new_infections_in )
     with concurrent.futures.ThreadPoolExecutor() as executor:
         results = list(executor.map(htbn, settings.nodes))
-        #results = list(executor.map(handle_new_infections_np, settings.nodes))
     return data_in
 
 def add_new_infections( data ):
     # Actually this just sets the new infection timers (globally) for all the new infections
     # New infections themselves are set node-wise
-    condition = np.logical_and(data['infected'], data['infection_timer'] == 0)
-    data['infection_timer'][condition] = np.random.randint(4, 15, size=np.sum(condition))
+    def add_new_infections_np( data ):
+        condition = np.logical_and(data['infected'], data['infection_timer'] == 0)
+        data['infection_timer'][condition] = np.random.randint(4, 15, size=np.sum(condition))
+        data['incubation_timer'][condition] = 2
+        return data
+    def add_new_infections_c( data ):
+        return data # already done
+    data = add_new_infections_c( data )
     return data
 
 def migrate( data, timestep, num_infected=None ):
+    # Migrate 1% of infecteds "downstream" every week; coz
     if timestep % 7 == 0: # every week
-        infected = np.where( data['infected'] )[0]
-        fraction = int(len(infected)*0.01)
-        selected = np.random.choice( infected, fraction )
-        # Update the 'nodes' array based on the specified conditions
-        data['node'][selected] = np.where(data['node'][selected] - 1 < 0, settings.num_nodes - 1, data['node'][selected] - 1 )
+        def migrate_np():
+            infected = np.where( data['infected'] )[0]
+            fraction = int(len(infected)*0.01)
+            selected = np.random.choice( infected, fraction )
+            # Update the 'nodes' array based on the specified conditions
+            data['node'][selected] = np.where(data['node'][selected] - 1 < 0, settings.num_nodes - 1, data['node'][selected] - 1 )
+        def migrate_c():
+            update_ages_lib.migrate(
+                len(data['age']),
+                data['infected'],
+                data['node'])
+        migrate_c()
     return data
 
 # Function to run the simulation for a given number of timesteps
 def run_simulation(data, csvwriter, num_timesteps):
-    currently_infectious, currently_sus, cur_reco  = report( data )
+    currently_infectious, currently_sus, cur_reco  = collect_report( data )
     report.write_timestep_report( csvwriter, 0, currently_infectious, currently_sus, cur_reco )
 
     for timestep in range(1, num_timesteps + 1):
@@ -196,7 +328,7 @@ def run_simulation(data, csvwriter, num_timesteps):
 
 # Main simulation
 if __name__ == "__main__":
-    data = load()
+    data = initialize_database()
 
     # Create a CSV file for reporting
     csvfile = open('simulation_report.csv', 'w', newline='') 
---
+++ sql_modeling/src/update_ages.c
@@ -0,0 +1,149 @@
+// update_ages.c
+
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+void update_ages(double *ages, size_t length) {
+    for (size_t i = 0; i < length; i++) {
+        ages[i] += 1.0 / 365.0;
+    }
+}
+
+void progress_infections(int n, float* infection_timer, float* incubation_timer, bool* infected, float* immunity_timer, bool* immunity) {
+    for (int i = 0; i < n; ++i) {
+        if (infected[i] ) {
+        // Infection timer: decrement for each infected person
+        if (infection_timer[i] >= 1) {
+            infection_timer[i] -= 1;
+
+        // Incubation timer: decrement for each person
+        if (incubation_timer[i] >= 1) {
+            incubation_timer[i] -= 1;
+        }
+
+            // Some people clear
+            if ( infection_timer[i] == 0) {
+                infected[i] = 0;
+
+            // Recovereds gain immunity
+                immunity_timer[i] = rand() % (41 - 10 + 1) + 10;  // Random integer between 10 and 40
+                immunity[i] = 1;
+            }
+        }
+        }
+    }
+}
+
+void progress_immunities(int n, float* immunity_timer, bool* immunity) {
+    for (int i = 0; i < n; ++i) {
+        if( immunity[i] && immunity_timer[i] > 0 )
+	{
+            immunity_timer[i]--;
+	    if( immunity_timer[i] == 0 )
+	    {
+	        immunity[i] = true;
+	    }
+	}	
+    }
+}
+
+// Dang, this one is slower than the numpy version!?!?
+// maybe I need to just use the 64-bit ints and avoid the casting
+void calculate_new_infections(
+    int num_agents,
+    int num_nodes,
+    float * incubation_timer,
+    uint32_t * node,
+    int64_t * infection_counts,
+    int64_t * sus,
+    uint32_t * new_infs_out
+) {
+    // We need number of infected not incubating
+    const float base_inf = 0.0001f;
+    int exposed_counts_by_bin[ num_nodes ];
+    memset( exposed_counts_by_bin, 0, sizeof(exposed_counts_by_bin) ); // not sure if this helps
+
+    for (int i = 0; i < num_agents; ++i) {
+	if( incubation_timer[i] >= 1 ) {
+	    exposed_counts_by_bin[ node[ i ] ] ++;
+	}
+    }
+
+    // new infections = Infecteds * infectivity * susceptibles
+    for (int i = 0; i < num_nodes; ++i) {
+	infection_counts[ i ] -= exposed_counts_by_bin[ i ];
+	float foi = infection_counts[ i ] * base_inf;
+	new_infs_out[ i ] = (int)( foi * sus[ i ] );
+    }
+}
+
+void handle_new_infections(
+    int num_agents,
+    int node,
+    uint32_t * agent_node,
+    bool * infected,
+    bool * immunity,
+    float * incubation_timer,
+    float * infection_timer,
+    int new_infections
+)
+{
+    // Create a boolean mask based on the conditions in the subquery
+    // This kind of sucks
+    bool* subquery_condition = (bool*)malloc(num_agents * sizeof(bool));
+
+    // Would be really nice to avoid looping over all the agents twice, especially since this is
+    // being called for each node (that has new infections).
+    for (int i = 0; i < num_agents; ++i) {
+        subquery_condition[i] = !infected[i] && !immunity[i] && agent_node[i] == node;
+    }
+
+    // Get the indices of eligible agents using the boolean mask
+    uint32_t* eligible_agents_indices = (uint32_t*)malloc(num_agents * sizeof(uint32_t));
+    int count = 0;
+
+    for (int i = 0; i < num_agents; ++i) {
+        if (subquery_condition[i]) {
+            eligible_agents_indices[count++] = i;
+        }
+    }
+    free(subquery_condition);
+
+    // Randomly sample 'new_infections' number of indices
+    uint32_t* selected_indices = (uint32_t*)malloc(new_infections * sizeof(uint32_t));
+
+    for (uint32_t i = 0; i < new_infections && i < count; ++i) {
+        int random_index = rand() % count;
+	// TBD: This needs to be change to avoid duplicates (with replacement or whatever)
+        selected_indices[i] = eligible_agents_indices[random_index];
+    }
+
+    // Update the 'infected' column based on the selected indices
+    for (uint32_t i = 0; i < new_infections && i < count; ++i) {
+        infected[selected_indices[i]] = 1;  // Assuming 1 represents True
+        incubation_timer[selected_indices[i]] = 2; 
+        infection_timer[selected_indices[i]] = rand() % (10) + 4; // Random integer between 4 and 14;
+    }
+
+    // Free allocated memory
+    free(eligible_agents_indices);
+    free(selected_indices);
+}
+
+void migrate( int num_agents, bool * infected, uint32_t * node ) {
+    int fraction = (int)(0.01f*100);
+    unsigned int counter = 0;
+    for (int i = 0; i < num_agents; ++i) {
+	if( infected[ i ] && rand()%100 < fraction )
+	{
+	    if( node[ i ] > 0 )
+	    {
+		node[ i ] --;
+	    }
+	}
+    }
+}
