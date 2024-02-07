78db3b993d8db4b4501857b40850841c4ca56a65
Jonathan Bloedow <jhb4@cornell.edu>
2024-02-06 16:22:22 -0800

    Assume mcw column is history at this point.

--- sql_modeling/src/csv_to_db.py
+++ sql_modeling/src/csv_to_db.py
@@ -4,7 +4,8 @@ import pandas as pd
 import sys
 
 df = pd.read_csv( sys.argv[1] )
-df = df.drop(columns=['mcw'])
+#make this conditional
+#df = df.drop(columns=['mcw'])
 conn=sql.connect("eula.db")
 df.to_sql("agents",conn,index=False)
 
