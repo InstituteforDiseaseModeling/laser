58888a47029d95744d28d620e5bda3dcb271e271
Jonathan Bloedow <jhb4@cornell.edu>
2024-02-06 16:21:07 -0800

    Copy csv header to both less-than and greater-than eula split files.

--- sql_modeling/src/eula_preproc.sh
+++ sql_modeling/src/eula_preproc.sh
@@ -9,13 +9,21 @@ fi
 # Assign the filename and threshold to variables
 filename=$1
 threshold=$2
+# Save the header line to a variable
+header=$(head -n 1 "$filename")
 
 # Run awk to split the file based on the age column and the provided threshold
-awk -v threshold="$threshold" -F ',' '{
-    if ($3 > threshold) {
-        print > "age_gt_" threshold ".csv"
-    } else if ($3 < threshold) {
-        print > "age_lt_" threshold ".csv"
+awk -v threshold="$threshold" -v header="$header" -F ',' '{
+    if (NR == 1) {
+        # Print the header to both output files
+        print header > "age_gt_" threshold ".csv"
+        print header > "age_lt_" threshold ".csv"
+    } else {
+        if ($3 > threshold) {
+            print > "age_gt_" threshold ".csv"
+        } else if ($3 < threshold) {
+            print > "age_lt_" threshold ".csv"
+        }
     }
 }' "$filename"
 
