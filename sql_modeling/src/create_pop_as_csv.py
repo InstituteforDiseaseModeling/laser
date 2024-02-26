359f74a355293cc4cfca47d6e9077755558138dc
Jonathan Bloedow <jhb4@cornell.edu>
2024-02-26 15:14:49 -0800

    Create modeled pop and eula binned csvs directly during creation instead of pipelining. For really big populations, the intermedidate csv creation is too slow.

--- sql_modeling/src/create_pop_as_csv.py
+++ sql_modeling/src/create_pop_as_csv.py
@@ -3,18 +3,48 @@ import csv
 import settings
 from sir_sql import initialize_database
 
+# 1) Create a full population in a SQLite db in memory
 conn = sqlite3.connect(":memory:")  # Use in-memory database for simplicity
 initialize_database( conn, from_file=False )
 
-print( f"Writing population file out out to csv: {settings.pop_file_out}." )
+# 2) Convert the modeled population into a csv file
+print( f"Writing population file out out to csv: modeled_pop.csv." )
 cursor = conn.cursor()
-get_all_query = "SELECT * FROM agents ORDER BY age"
+get_all_query = f"SELECT * FROM agents WHERE age>{settings.eula_age} ORDER BY age"
+
 cursor.execute( get_all_query )
+rows = cursor.fetchall()
+
+
+with open( "modeled_pop.csv", "w", newline='' ) as csvfile:
+    csv_writer = csv.writer( csvfile )
+    csv_writer.writerow( ['id', 'node', 'age', 'infected', 'infection_timer', 'incubation_timer', 'immunity', 'immunity_timer', 'expected_lifespan' ] )
+    csv_writer.writerows( rows )
+
+get_eula_query = f"SELECT node, FLOOR(age) AS age, COUNT(*) AS total_individuals FROM agents GROUP BY node, FLOOR(age) ORDER BY node, age"
+cursor.execute( get_eula_query )
 rows = cursor.fetchall()
 
 conn.close()
 
-with open( settings.pop_file_out, "w", newline='' ) as csvfile:
-    csv_writer = csv.writer( csvfile )
-    csv_writer.writerow( ['id', 'node', 'age', 'infected', 'infection_timer', 'incubation_timer', 'immunity', 'immunity_timer', 'expected_lifespan' ] )
-    csv_writer.writerows( rows )
+result_dict = [] # dict=array
+# Iterate over the fetched rows and construct dictionaries
+for row in rows:
+    result_dict.append({
+        "node": row[0],
+        "age": row[1],
+        "total": row[2]
+    })
+
+# Specify the CSV file path
+csv_file_path = "eula_binned.csv"
+
+# Write the dictionary data to a CSV file
+with open(csv_file_path, mode='w', newline='') as csv_file:
+    fieldnames = ["node", "age", "total"]
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+    writer.writeheader()
+    for row in result_dict:
+        writer.writerow(row)
+
