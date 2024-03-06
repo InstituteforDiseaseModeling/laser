e0a67ff790f950c71459f100a27e9f0a4d40234d
Jonathan Bloedow <jhb4@cornell.edu>
2024-03-05 17:13:04 -0800

    Set fertility and mortality intervals to 1 since they are well optimized now.

--- sql_modeling/src/settings.py
+++ sql_modeling/src/settings.py
@@ -6,20 +6,16 @@ nodes = [ x for x in range(num_nodes) ]
 eula_age=5
 
 # Filenames if loading pop from file
-#pop_file="pop_1M_25nodes_seeded.csv"
-#pop_file="age_lt_5.csv"
 pop_file="modeled_pop.csv"
 eula_file="eula_binned.csv"
-#pop_file_out=f"pop_{int(pop/1e6)}M_{num_nodes}nodes_seeded.csv"
-#pop_file_out=f"pop_{int(pop/1e6)}M_seeded.csv"
-pop_file_out=f"pop_seeded.csv"
+#pop_file_out=f"pop_seeded.csv"
 eula_pop_fits="fits.npy"
 
 report_filename="simulation_output.csv"
 
 # numerical runtime config params
 # simulation duration
-duration = 20*365 # 900
+duration = 20*365
 #base_infectivity = 2e7
 base_infectivity = 1.5e7
 # cbr=crude bith rate
@@ -31,6 +27,6 @@ campaign_day=60
 campaign_coverage=0.75
 campaign_node=15
 migration_interval=7
-mortality_interval=30
-fertility_interval=14
+mortality_interval=1 # 30
+fertility_interval=1 # 7
 ria_interval=7
