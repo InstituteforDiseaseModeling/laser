af5ab7f601fe75f58bcd5fb01249778d0b3c2cb2
Christopher Lorton <christopher.lorton@gatesfoundation.org>
2024-06-06 15:24:59 -0700

    ✨ add predicted date of (non-disease) death

--- src/idmlaser/utils.py
+++ src/idmlaser/utils.py
@@ -142,3 +142,181 @@ class NumpyJSONEncoder(JSONEncoder):
         if isinstance(obj, Path):
             return str(obj)
         return JSONEncoder.default(self, obj)
+
+
+# Derived from table 1 of "National Vital Statistics Reports Volume 54, Number 14 United States Life Tables, 2003" (see README.md)
+cumulative_deaths = [
+    0,
+    687,
+    733,
+    767,
+    792,
+    811,
+    829,
+    844,
+    859,
+    872,
+    884,
+    895,
+    906,
+    922,
+    945,
+    978,
+    1024,
+    1081,
+    1149,
+    1225,
+    1307,
+    1395,
+    1489,
+    1587,
+    1685,
+    1781,
+    1876,
+    1968,
+    2060,
+    2153,
+    2248,
+    2346,
+    2449,
+    2556,
+    2669,
+    2790,
+    2920,
+    3060,
+    3212,
+    3377,
+    3558,
+    3755,
+    3967,
+    4197,
+    4445,
+    4715,
+    5007,
+    5322,
+    5662,
+    6026,
+    6416,
+    6833,
+    7281,
+    7759,
+    8272,
+    8819,
+    9405,
+    10032,
+    10707,
+    11435,
+    12226,
+    13089,
+    14030,
+    15051,
+    16146,
+    17312,
+    18552,
+    19877,
+    21295,
+    22816,
+    24445,
+    26179,
+    28018,
+    29972,
+    32058,
+    34283,
+    36638,
+    39108,
+    41696,
+    44411,
+    47257,
+    50228,
+    53306,
+    56474,
+    59717,
+    63019,
+    66336,
+    69636,
+    72887,
+    76054,
+    79102,
+    81998,
+    84711,
+    87212,
+    89481,
+    91501,
+    93266,
+    94775,
+    96036,
+    97065,
+    97882,
+    100000,
+]
+
+__cdnp = np.array(cumulative_deaths)
+
+
+def predicted_year_of_death(age_years, max_year=100):
+    """
+    Calculates the predicted year of death based on the given age in years.
+
+    Parameters:
+    - age_years (int): The age of the individual in years.
+    - max_year (int): The maximum year to consider for calculating the predicted year of death. Default is 100.
+
+    Returns:
+    - yod (int): The predicted year of death.
+
+    Example:
+    >>> predicted_year_of_death(40, max_year=80)
+    62
+    """
+
+    # e.g., max_year == 10, 884 deaths are recorded in the first 10 years
+    total_deaths = __cdnp[max_year + 1]
+    # account for current age, i.e., agent is already 4 years old, so 792 deaths have already occurred
+    already_deceased = __cdnp[age_years]
+    # this agent will be one of the deaths in (already_deceased, total_deaths] == [already_deceased+1, total_deaths+1)
+    draw = np.random.randint(already_deceased + 1, total_deaths + 1)
+    # find the year of death, e.g., draw == 733, searchsorted("left") will return 2, so the year of death is 1
+    yod = np.searchsorted(__cdnp, draw, side="left") - 1
+
+    return yod
+
+
+def predicted_day_of_death(age_days, max_year=100):
+    """
+    Calculates the predicted day of death based on the given age in days and the maximum year of death.
+
+    Parameters:
+    - age_days (int): The age in days.
+    - max_year (int): The maximum year of death. Defaults to 100.
+
+    Returns:
+    - dod (int): The predicted day of death.
+
+    The function first calculates the predicted year of death based on the given age in days and the maximum year of death.
+    Then, it randomly selects a day within the year of death.
+    The age/date of death has to be greater than today's age.
+    Finally, it calculates and returns the predicted day of death.
+
+    Note: This function assumes that there are 365 days in a year.
+    """
+
+    yod = predicted_year_of_death(age_days // 365, max_year)
+
+    # if the death age year is not the current age year pick any day that year
+    if age_days // 365 < yod:
+        # the agent will die sometime in the year of death, so we randomly select a day
+        doy = np.random.randint(365)
+    else:
+        # the agent will die on or before next birthday
+        age_doy = age_days % 365  # 0 ... 364
+        if age_doy < 364:
+            # there is time before the next birthday, pick a day at random
+            doy = np.random.randint(age_doy + 1, 365)
+        else:
+            # the agent's birthday is tomorrow; bummer of a birthday present
+            yod += 1
+            doy = 0
+
+    dod = yod * 365 + doy
+
+    return dod
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,6 +8,8 @@ import pytest
 from idmlaser.utils import PriorityQueue
 from idmlaser.utils import daily_births_deaths_from_annual
 from idmlaser.utils import pop_from_cbr_and_mortality
+from idmlaser.utils import predicted_day_of_death
+from idmlaser.utils import predicted_year_of_death
 
 
 class TestPriorityQueue(unittest.TestCase):
@@ -168,6 +170,140 @@ class TestDailyBirthsDeathsFromAnnual(unittest.TestCase):
         assert daily_deaths.sum() == annual_deaths.sum(), f"Expected {annual_deaths.sum()}, got {daily_deaths.sum()}"
         self.assertAlmostEqual(daily_births.mean(), annual_births.mean() / 365, places=4)  # noqa: PT009
         self.assertAlmostEqual(daily_deaths.mean(), annual_deaths.mean() / 365, places=4)  # noqa: PT009
+
+
+class TestPredictedDateOfDeath(unittest.TestCase):
+    def test_predicted_year_of_death_0_default(self):
+        age = 0
+        for _ in range(128):  # 100-ish trials
+            yod = predicted_year_of_death(age)
+            assert age <= yod <= 100, f"Expected {age} <= {yod} <= 100"
+
+    def test_predicted_year_of_death_0_100(self):
+        age = 0
+        max_year = 100
+        for _ in range(128):  # 100-ish trials
+            yod = predicted_year_of_death(age, max_year)
+            assert age <= yod <= max_year, f"Expected {age} <= {yod} <= {max_year}"
+
+    def test_predicted_year_of_death_0_89(self):
+        age = 0
+        max_year = 89
+        for _ in range(128):  # 100-ish trials
+            yod = predicted_year_of_death(age, max_year)
+            assert age <= yod <= max_year, f"Expected {age} <= {yod} <= {max_year}"
+
+    def test_predicted_year_of_death_30_default(self):
+        age = 30
+        for _ in range(128):  # 100-ish trials
+            yod = predicted_year_of_death(age)
+            assert age <= yod <= 100, f"Expected {age} <= {yod} <= 100"
+
+    def test_predicted_year_of_death_30_100(self):
+        age = 30
+        max_year = 100
+        for _ in range(128):  # 100-ish trials
+            yod = predicted_year_of_death(age, max_year)
+            assert age <= yod <= max_year, f"Expected {age} <= {yod} <= {max_year}"
+
+    def test_predicted_year_of_death_30_89(self):
+        age = 30
+        max_year = 89
+        for _ in range(128):  # 100-ish trials
+            yod = predicted_year_of_death(age, max_year)
+            assert age <= yod <= max_year, f"Expected {age} <= {yod} <= {max_year}"
+
+    def test_predicted_year_of_death_default_maximum(self):
+        age = 100
+        for _ in range(128):
+            yod = predicted_year_of_death(age)
+            assert yod == 100, f"Expected {yod} == 100"
+
+    def test_predicted_year_of_death_30_maximum(self):
+        age = max_year = 30
+        for _ in range(128):
+            yod = predicted_year_of_death(age, max_year)
+            assert yod == max_year, f"Expected {yod} == {max_year}"
+
+    def test_predicted_day_of_death_0_default(self):
+        age = 0
+        for _ in range(128):  # 100-ish trials
+            dod = predicted_day_of_death(age)
+            assert age < dod < 36865, f"Expected {age} < {dod} < 36,865 (365 * 100 + 365)"
+
+    def test_predicted_day_of_death_0_100(self):
+        age = 0
+        max_year = 100
+        for _ in range(128):  # 100-ish trials
+            dod = predicted_day_of_death(age, max_year)
+            assert age < dod < (max_year * 365 + 365), f"Expected {age} < {dod} < {(max_year * 365 + 365)}"
+
+    def test_predicted_day_of_death_0_89(self):
+        age = 0
+        max_year = 89
+        for _ in range(128):  # 100-ish trials
+            dod = predicted_day_of_death(age, max_year)
+            assert age < dod < (max_year * 365 + 365), f"Expected {age} < {dod} < {(max_year * 365 + 365)}"
+
+    def test_predicted_day_of_death_30_default(self):
+        age = 30 * 365 + 180  # ~30 1/2 years
+        for _ in range(128):  # 100-ish trials
+            dod = predicted_day_of_death(age)
+            assert age < dod < 36865, f"Expected {age} < {dod} < 36,865"
+
+    def test_predicted_day_of_death_30_100(self):
+        age = 30 * 365 + 180  # ~30 1/2 years
+        max_year = 100
+        for _ in range(128):  # 100-ish trials
+            dod = predicted_day_of_death(age, max_year)
+            assert age < dod < (max_year * 365 + 365), f"Expected {age} < {dod} < {(max_year * 365 + 365)}"
+
+    def test_predicted_day_of_death_30_89(self):
+        age = 30 * 365 + 180  # ~30 1/2 years
+        max_year = 89
+        for _ in range(128):  # 100-ish trials
+            dod = predicted_day_of_death(age, max_year)
+            assert age < dod < (max_year * 365 + 365), f"Expected {age} < {dod} < {(max_year * 365 + 365)}"
+
+    def test_predicted_day_of_death_default_maximum(self):
+        age = 100 * 365  # 100 years
+        for _ in range(128):
+            dod = predicted_day_of_death(age)
+            assert dod // 365 == 100, f"Expected {dod} // 365 == 100"
+            assert (dod - age) < 365, f"Expected {dod} - {age} ({dod-age}) < 365"
+
+    def test_predicted_day_of_death_30_maximum(self):
+        max_year = 30
+        age = max_year * 365 + 180  # ~30 1/2 years
+        for _ in range(128):
+            dod = predicted_day_of_death(age, max_year)
+            assert dod // 365 == max_year, f"Expected {dod} // 365 == {max_year}"
+            assert (dod - age) < 365, f"Expected {dod} - {age} ({dod-age}) < 365"
+
+    def test_doy_distribution(self):
+        for _ in range(128):
+            age_days = np.random.randint(0, 365 * 100)
+            dod = predicted_day_of_death(age_days)
+            if age_days % 365 == 364:
+                assert dod % 365 == 0, f"Expected {dod} % 365 == 0"
+            elif dod // 365 != age_days // 365:
+                assert dod % 365 in range(365), f"Expected {dod} % 365 in range(0, 365)"
+            else:
+                assert dod % 365 in range(age_days % 365 + 1, 365), f"Expected {dod} % 365 in range({age_days % 365 + 1}, 365)"
+
+    def test_predicted_day_of_death_30plus363_30max(self):
+        for max_year in [5, 10, 15, 20, 25, 30]:
+            age = max_year * 365 + 363  # "December 30th", so to speak
+            dod = predicted_day_of_death(age, max_year)
+            assert dod // 365 == max_year, f"Expected {dod} // 365 == {max_year}"
+            assert dod % 365 == 364, f"Expected {dod} % 365 == 364"  # "December 31st", so to speak
+
+    def test_predicted_day_of_death_30plus364_30max(self):
+        for max_year in [5, 10, 15, 20, 25, 30]:
+            age = max_year * 365 + 364  # "December 31st", so to speak
+            dod = predicted_day_of_death(age, max_year)
+            assert dod // 365 == max_year + 1, f"Expected {dod} // 365 == {max_year + 1}"
+            assert dod % 365 == 0, f"Expected {dod} % 365 == 364"  # "January 1st", so to speak
 
 
 if __name__ == "__main__":
