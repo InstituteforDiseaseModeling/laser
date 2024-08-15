940c54b0d5237fdc638d324c8962a505caa2d48c
Jonathan Bloedow <jhb4@cornell.edu>
2024-08-15 10:27:55 -0700

    Moved agent population schema into 'data' file.

---
+++ nnmm/schema.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+schema = {
+    "nodeid": np.uint16,
+    "dob": np.int32,
+    "dod": np.int32,
+    "susceptibility": np.uint8,
+    "ri_timer": np.uint16,
+    "etimer": np.uint8,
+    "itimer": np.uint8,
+    "susceptibility_timer": np.uint8,
+    "accessibility": np.uint8
+}
+
