da3519fa8d48ddfb1539bb138f2141de7b89d3dd
Jonathan Bloedow <jhb4@cornell.edu>
2024-10-10 09:41:59 -0700

    Adding jp notebook with plots using different values of 'delta' in transmission time-slicing.

--- src/idmlaser_cholera/mods/intrahost.py
+++ src/idmlaser_cholera/mods/intrahost.py
@@ -1,5 +1,51 @@
 import numba as nb
 import numpy as np
+import ctypes
+from pkg_resources import resource_filename
+
+use_nb = True
+lib = None
+delta = 2
+
+def init( model ):
+    global use_nb, lib
+    try:
+    # Load the shared library
+        shared_lib_path = resource_filename('idmlaser_cholera', 'mods/libintrahost.so')
+        lib = ctypes.CDLL(shared_lib_path)
+
+        # Define the argument types for the C function
+        lib.exposure_update.argtypes = [
+            ctypes.c_uint32,               # count
+            np.ctypeslib.ndpointer(dtype=np.uint8),    # etimer
+            np.ctypeslib.ndpointer(dtype=np.uint8),    # itimer
+            ctypes.c_float,                # inf_mean
+            ctypes.c_float,                # inf_std
+            ctypes.c_uint32,               # tick
+            ctypes.c_uint32                # delta
+        ]
+        
+        lib.infection_update.argtypes = [
+            ctypes.c_uint32,               # count
+            np.ctypeslib.ndpointer(dtype=np.uint8),    # itimer
+            np.ctypeslib.ndpointer(dtype=np.uint16),   # sus_timer
+            np.ctypeslib.ndpointer(dtype=np.uint8),    # sus
+            ctypes.c_uint32,               # tick
+            ctypes.c_uint32                # delta
+        ]
+
+        lib.infection_update_contiguous.argtypes = [
+            ctypes.c_uint32,               # count
+            np.ctypeslib.ndpointer(dtype=np.uint8),    # itimer
+            np.ctypeslib.ndpointer(dtype=np.uint16),   # sus_timer
+            np.ctypeslib.ndpointer(dtype=np.uint8),    # sus
+            ctypes.c_uint32,               # tick
+            ctypes.c_uint32                # delta
+        ]
+        use_nb = False
+    except Exception as ex:
+        print( "Couldn't load exposure_update.so. Using numba." )
+
 
 # ## Incubation and Infection
 # 
@@ -12,25 +58,40 @@ import numpy as np
 # In[13]:
 
 
-@nb.njit((nb.uint32, nb.uint8[:], nb.uint8[:], nb.float32, nb.float32), parallel=True)
-def _exposure_update(count, etimers, itimers, inf_mean, inf_std):
-    for i in nb.prange(count):
-        if etimers[i] > 0:
-            etimers[i] -= 1
-            if etimers[i] == 0:
-                itimers[i] = np.maximum(np.uint8(1), np.uint8(np.round(np.random.normal(inf_mean, inf_std))))    # must be at least 1 day
+@nb.njit((
+    nb.uint32,
+    nb.uint8[:],
+    nb.uint8[:],
+    nb.uint16[:],
+    nb.uint8[:],
+    nb.float32, 
+    nb.float32,
+    nb.uint32),
+    parallel=True)
+def _exposure_update(count, etimer, itimer, sus_timer, sus, inf_mean, inf_std, tick):
+    #start_index = tick % 2
 
+    #for i in nb.prange(start_index, count, 2):
+    for i in nb.prange(count):
+        if etimer[i] > 0:
+            etimer[i] -= 1
+            if etimer[i] == 0:
+                itimer[i] = np.maximum(np.uint8(1), np.uint8(np.round(np.random.normal(inf_mean, inf_std))))    # must be at least 1 day
     return
 
 def do_exposure_update(model, tick):
 
-    _exposure_update(nb.uint32(model.population.count), model.population.etimer, model.population.itimer, model.params.inf_mean, model.params.inf_std)
+    #global lib
+    #if use_nb:
+    _exposure_update(nb.uint32(model.population.count), model.population.etimer, model.population.itimer, model.population.susceptibility_timer, model.population.susceptibility, model.params.inf_mean, model.params.inf_std, tick)
+    #else:
+    #    lib.exposure_update(model.population.count, model.population.etimer, model.population.itimer, model.params.inf_mean, model.params.inf_std, tick, delta)
 
     return
 
 
-@nb.njit((nb.uint32, nb.uint8[:], nb.uint16[:], nb.uint8[:]), parallel=True)
-def _infection_update(count, itimer, sus_timer, susceptibility):
+@nb.njit((nb.uint32, nb.uint8[:], nb.uint16[:], nb.uint8[:], nb.uint32 ), parallel=True)
+def _infection_update(count, itimer, sus_timer, susceptibility, tick):
     """
     Updates infection and susceptibility timers for a population of agents.
 
@@ -60,7 +121,8 @@ def _infection_update(count, itimer, sus_timer, susceptibility):
     """
     for i in nb.prange(count):
         if itimer[i] > 0:
-            itimer[i] -= 1
+            #itimer[i] -= 1
+            itimer[i] = max( itimer[i]-delta, 0 )
             if itimer[i] <= 0:
                 susceptibility[i] = 0
                 sus_timer[i] = np.random.randint(2*365, 4*365) # waning
@@ -69,7 +131,12 @@ def _infection_update(count, itimer, sus_timer, susceptibility):
 
 def do_infection_update(model, tick):
 
-    _infection_update(nb.uint32(model.population.count), model.population.itimer, model.population.susceptibility_timer, model.population.susceptibility)
+    global lib
+    if True: # use_nb:
+        if tick % delta == 0:
+            _infection_update(nb.uint32(model.population.count), model.population.itimer, model.population.susceptibility_timer, model.population.susceptibility, tick)
+    else:
+        lib.infection_update_contiguous(model.population.count, model.population.itimer, model.population.susceptibility_timer, model.population.susceptibility, tick, delta)
 
     return
 
