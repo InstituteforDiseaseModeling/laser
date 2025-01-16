c6423a9d859f2541df4e47feb7f42cf2e089c8a9
Jonathan Bloedow <jonathan.bloedow@gatesfoundation.org>
2025-01-15 16:26:35 -0800

    Bump version: 0.1.1 → 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ project = "LASER"
 year = "2023-2024"
 author = "Institute for Disease Modeling"
 copyright = f"{year}, {author}"
-version = release = "0.1.1"
+version = release = "0.2.0"
 
 pygments_style = "trac"
 templates_path = ["."]
