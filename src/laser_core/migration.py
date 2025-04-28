18ce44ef95db62c180d94f45f01d8eb855b04d3b
Christopher Lorton <christopher.lorton@gatesfoundation.org>
2025-04-28 20:12:28 +0000

    🐛 update row_normalizer() to return float32 If the incoming network is an integer type and the max rowsum is fractional (common), the returned values would be zero.

--- src/laser_core/migration.py
+++ src/laser_core/migration.py
@@ -146,6 +146,10 @@ def row_normalizer(network, max_rowsum):
     _is_instance(max_rowsum, Number, f"max_rowsum must be a numeric value ({type(max_rowsum)=})")
     _has_values(0 <= max_rowsum <= 1, "max_rowsum must be in [0, 1]")
 
+    # Promote to network to floating point
+    # If the incoming network is an integer type and max_rowsum is < 1.0, the result is all zeros.
+    network = network.copy().astype(np.float32)
+
     rowsums = network.sum(axis=1)
     rows_to_renorm = rowsums > max_rowsum
     network[rows_to_renorm] = network[rows_to_renorm] * max_rowsum / rowsums[rows_to_renorm, np.newaxis]
