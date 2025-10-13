52c86b473c4e6d433368a3d96b70e61125595a99
Christopher Lorton <christopher.lorton@gatesfoundation.org>
2025-10-13 12:47:57 -0700

    make linter happy

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import Extension
 from setuptools import find_packages
 from setuptools import setup
 
-
 setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
